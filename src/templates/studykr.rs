6499c01f3919e5d64350a4aef3d38bbf7dcfc2fc
Global Young <67513038+YoungHaKim7@users.noreply.github.com>
2023-12-12 09:49:58 +0900

    Update studykr.rs

--- src/templates/studykr.rs
+++ src/templates/studykr.rs
@@ -6,8 +6,8 @@ fn studykr_page<G: Html>(cx: Scope) -> View<G> {
         // div
         div(style = "display: flex; flex-direction: column; justify-content: center; align-items: center; height: 95vh;") {
             h1 { "Study! Study" }
-            p { "KakaoTalk Korea. Rust Programming"}
-            a(href = "https://open.kakao.com/o/g6eesURb", id = "study-link") { "Kor_rust_study group!"}
+            p { "Discord. Rust Programming"}
+            a(href = "https://", id = "study-link") { "_rust_study group!"}
             p { "_______________."}
             p { "Discord Korea."}
             a(href = "https://discord.gg/GKXNDdn2CP", id = "study-link") { "Kor_rust_study group!"}
